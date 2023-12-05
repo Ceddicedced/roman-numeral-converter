0bde09d1c8151066b22aed4404d479422c3fa831
ceddicedced <cedric@busacker.dev>
2023-12-05 21:20:21 +0100

    # Refactored cli into __main__ and converter into extra file

--- roman_numerals/roman.py
+++ roman_numerals/roman.py
@@ -4,107 +4,124 @@ This script provides a command-line interface to convert numbers to Roman numera
 
 import re
 
-import click
-
 # Regular expression for validating Roman numerals
 ROMAN_REGEX = r"^(?=.)M{0,3}(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$"
 
 
-@click.group()
-def cli() -> None:
-    """Click group for the CLI.
-    This group bundles the commands for converting to and from Roman numerals.
-    """
+class RomanError(Exception):
+    """Base class for exceptions in this module."""
+
     pass
 
 
-@cli.command()
-@click.argument("number", type=int)
-def to_roman(number: int) -> None:
+class RomanNumeral:
+    """Class for converting Roman numerals to decimal numbers and vice versa.
+    This class provides a constructor for creating a RomanNumeral object from a
+    Roman numeral or a decimal number. It also provides methods for converting
+    a Roman numeral to a decimal number and vice versa.
     """
-    Convert a number to a Roman numeral.
 
-    Args:
-        number (int): A decimal number to be converted into Roman numeral.
+    def __init__(self, value: str) -> None:
+        """Constructor for RomanNumeral class.
+        Args:
+            value (str): A Roman numeral or a decimal number.
+        """
+        # Validate input
+        if not isinstance(value, str):
+            raise RomanError("Please enter a string.")
 
-    This function takes an integer and converts it into its Roman numeral representation.
-    It checks if the number is within the permissible range (1-3999) and then converts it
-    using defined Roman numeral mappings.
-    """  # noqa: E501
-    # Check that the number is within the valid range
-    if not 0 < number < 4000:
-        raise click.BadParameter("Please enter a number between 1 and 3999.")
+        # Check if the input is a Roman numeral
+        if not self.is_valid_roman(value):
+            raise RomanError("Please enter a valid Roman numeral.")
 
-    # Mapping of decimal numbers to Roman numerals
-    roman_numerals = [
-        (1000, "M"),
-        (900, "CM"),
-        (500, "D"),
-        (400, "CD"),
-        (100, "C"),
-        (90, "XC"),
-        (50, "L"),
-        (40, "XL"),
-        (10, "X"),
-        (9, "IX"),
-        (5, "V"),
-        (4, "IV"),
-        (1, "I"),
-    ]
+        self._value = value
 
-    # Conversion process
-    roman = ""
-    for value, numeral in roman_numerals:
-        while number >= value:
-            roman += numeral
-            number -= value
+    def to_decimal(self) -> int:
+        """Convert a Roman numeral to a decimal number.
+        Returns:
+            int: The decimal representation of the Roman numeral.
+        """
 
-    click.echo(roman)
+        # Mapping of Roman numerals to decimal numbers
+        roman_numerals = [
+            ("M", 1000),
+            ("CM", 900),
+            ("D", 500),
+            ("CD", 400),
+            ("C", 100),
+            ("XC", 90),
+            ("L", 50),
+            ("XL", 40),
+            ("X", 10),
+            ("IX", 9),
+            ("V", 5),
+            ("IV", 4),
+            ("I", 1),
+        ]
 
+        # Conversion process
+        decimal = 0
+        for numeral, value in roman_numerals:
+            while self._value.startswith(numeral):
+                decimal += value
+                self._value = self._value[len(numeral) :]
 
-@cli.command()
-@click.argument("roman", type=str)
-def from_roman(roman: str) -> None:
-    """
-    Convert a Roman numeral to a number.
+        return decimal
 
-    Args:
-        roman (str): A Roman numeral to be converted into a decimal number.
+    def __str__(self) -> str:
+        """Convert a Roman numeral to a string.
+        Returns:
+            str: The Roman numeral representation of the object.
+        """
+        return self._value
 
-    This function takes a Roman numeral and converts it into its decimal representation.
-    It first validates the Roman numeral using a regular expression and then performs
-    the conversion using defined mappings from Roman numerals to decimal numbers.
-    """
-    # Validate Roman numeral input
-    if not re.search(ROMAN_REGEX, roman):
-        raise click.BadParameter("Please enter a valid Roman numeral.")
+    @staticmethod
+    def is_valid_roman(value: str) -> bool:
+        """Check if a string is a valid Roman numeral.
+        Args:
+            value (str): A string to be checked.
+        Returns:
+            bool: True if the string is a valid Roman numeral, False otherwise.
+        """
+        return bool(re.search(ROMAN_REGEX, value) is not None)
 
-    # Mapping of Roman numerals to decimal numbers
-    roman_numerals: dict[str, int] = {
-        "CM": 900,
-        "CD": 400,
-        "XC": 90,
-        "XL": 40,
-        "IX": 9,
-        "IV": 4,
-        "M": 1000,
-        "D": 500,
-        "C": 100,
-        "L": 50,
-        "X": 10,
-        "V": 5,
-        "I": 1,
-    }
+    @classmethod
+    def from_decimal(cls, number: int) -> "RomanNumeral":
+        """Convert a decimal number to a Roman numeral.
+        Args:
+            number (int): A decimal number.
+        Returns:
+            str: The Roman numeral representation of the decimal number.
+        """
+        if not isinstance(number, int):
+            raise RomanError("Please enter an integer.")
 
-    # Conversion process
-    num = 0
-    for key, value in roman_numerals.items():
-        if key in roman:
-            num += value * roman.count(key)
-            roman = roman.replace(key, "")
+        # Check that the number is within the valid range
+        if not 0 < number < 4000:
+            raise RomanError("Please enter a number between 1 and 3999.")
 
-    click.echo(num)
+        # Mapping of decimal numbers to Roman numerals
+        roman_numerals = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
 
+        # Conversion process
+        roman = ""
+        for value, numeral in roman_numerals:
+            while number >= value:
+                roman += numeral
+                number -= value
 
-if __name__ == "__main__":
-    cli()
+        return cls(roman)
