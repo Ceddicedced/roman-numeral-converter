4f59279c8b283eec59a7dbe062a6dff5acb110ab
ceddicedced <cedric@busacker.dev>
2023-12-05 21:20:33 +0100

    # Add unit tests for RomanNumeral class

---
+++ tests/test_cli.py
@@ -0,0 +1,77 @@
+import pytest
+from click.testing import CliRunner
+
+from roman_numerals import cli
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+def test_to_roman(runner: CliRunner) -> None:
+    # Test conversion of a few numbers to Roman numerals
+    test_cases = [
+        (1, "I"),
+        (4, "IV"),
+        (9, "IX"),
+        (12, "XII"),
+        (50, "L"),
+        (100, "C"),
+        (500, "D"),
+        (1000, "M"),
+        (3999, "MMMCMXCIX"),
+    ]
+
+    for number, expected_output in test_cases:
+        result = runner.invoke(cli, ["to-roman", str(number)])
+        assert result.exit_code == 0
+        assert result.output.strip() == expected_output
+
+
+def test_from_roman(runner: CliRunner):
+    # Test conversion of a few Roman numerals to numbers
+    test_cases = [
+        ("I", 1),
+        ("IV", 4),
+        ("IX", 9),
+        ("XII", 12),
+        ("L", 50),
+        ("C", 100),
+        ("D", 500),
+        ("M", 1000),
+        ("MMMCMXCIX", 3999),
+    ]
+
+    for roman, expected_output in test_cases:
+        result = runner.invoke(cli, ["from-roman", roman])
+        assert result.exit_code == 0
+        assert result.output.strip() == str(expected_output)
+
+
+def test_to_roman_invalid_input(runner: CliRunner):
+    # Test invalid integer input
+    invalid_inputs = [-1, 0, 4000, 1.5, "abc"]
+    for number in invalid_inputs:
+        result = runner.invoke(cli, ["to-roman", str(number)])
+        assert result.exception
+
+
+def test_from_roman_invalid_input(runner: CliRunner):
+    # Test invalid Roman numeral input
+    invalid_inputs = ["IIII", "VV", "LL", "DD", "MMMM", "ABC"]
+    for roman in invalid_inputs:
+        result = runner.invoke(cli, ["from-roman", roman])
+        assert result.exception
+
+
+def test_equivalence(runner: CliRunner):
+    # Test random equivalence between to-roman and from-roman
+
+    for number in range(1, 3999):
+        result = runner.invoke(cli, ["to-roman", str(number)])
+        assert result.exit_code == 0
+        roman = result.output.strip()
+        result = runner.invoke(cli, ["from-roman", roman])
+        assert result.exit_code == 0
+        assert result.output.strip() == str(number)
--- tests/test_roman.py
+++ tests/test_roman.py
@@ -1,7 +1,7 @@
 import pytest
 from click.testing import CliRunner
 
-from roman_numerals.roman import cli
+from roman_numerals import RomanError, RomanNumeral
 
 
 @pytest.fixture
@@ -9,28 +9,8 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_to_roman(runner: CliRunner) -> None:
-    # Test conversion of a few numbers to Roman numerals
-    test_cases = [
-        (1, "I"),
-        (4, "IV"),
-        (9, "IX"),
-        (12, "XII"),
-        (50, "L"),
-        (100, "C"),
-        (500, "D"),
-        (1000, "M"),
-        (3999, "MMMCMXCIX"),
-    ]
-
-    for number, expected_output in test_cases:
-        result = runner.invoke(cli, ["to-roman", str(number)])
-        assert result.exit_code == 0
-        assert result.output.strip() == expected_output
-
-
-def test_from_roman(runner: CliRunner):
-    # Test conversion of a few Roman numerals to numbers
+def test_roman_numeral_to_decimal():
+    # Test conversion of Roman numerals to decimal numbers
     test_cases = [
         ("I", 1),
         ("IV", 4),
@@ -44,34 +24,59 @@ def test_from_roman(runner: CliRunner):
     ]
 
     for roman, expected_output in test_cases:
-        result = runner.invoke(cli, ["from-roman", roman])
-        assert result.exit_code == 0
-        assert result.output.strip() == str(expected_output)
+        roman_numeral = RomanNumeral(roman)
+        assert roman_numeral.to_decimal() == expected_output
 
 
-def test_to_roman_invalid_input(runner: CliRunner):
-    # Test invalid integer input
-    invalid_inputs = [-1, 0, 4000, 1.5, "abc"]
-    for number in invalid_inputs:
-        result = runner.invoke(cli, ["to-roman", str(number)])
-        assert result.exception
+def test_roman_numeral_to_string():
+    # Test conversion of Roman numerals to strings
+    test_cases = [
+        ("I", "I"),
+        ("IV", "IV"),
+        ("IX", "IX"),
+        ("XII", "XII"),
+        ("L", "L"),
+        ("C", "C"),
+        ("D", "D"),
+        ("M", "M"),
+        ("MMMCMXCIX", "MMMCMXCIX"),
+    ]
+
+    for roman, expected_output in test_cases:
+        roman_numeral = RomanNumeral(roman)
+        assert str(roman_numeral) == expected_output
 
 
-def test_from_roman_invalid_input(runner: CliRunner):
+def test_decimal_to_roman():
+    # Test conversion of decimal numbers to Roman numerals
+    test_cases = [
+        (1, "I"),
+        (4, "IV"),
+        (9, "IX"),
+        (12, "XII"),
+        (50, "L"),
+        (100, "C"),
+        (500, "D"),
+        (1000, "M"),
+        (3999, "MMMCMXCIX"),
+    ]
+
+    for decimal, expected_output in test_cases:
+        roman_numeral = RomanNumeral.from_decimal(decimal)
+        assert str(roman_numeral) == expected_output
+
+
+def test_invalid_roman_input():
     # Test invalid Roman numeral input
     invalid_inputs = ["IIII", "VV", "LL", "DD", "MMMM", "ABC"]
     for roman in invalid_inputs:
-        result = runner.invoke(cli, ["from-roman", roman])
-        assert result.exception
+        with pytest.raises(RomanError):
+            RomanNumeral(roman)
 
 
-def test_equivalence(runner: CliRunner):
-    # Test random equivalence between to-roman and from-roman
-
-    for number in range(1, 3999):
-        result = runner.invoke(cli, ["to-roman", str(number)])
-        assert result.exit_code == 0
-        roman = result.output.strip()
-        result = runner.invoke(cli, ["from-roman", roman])
-        assert result.exit_code == 0
-        assert result.output.strip() == str(number)
+def test_invalid_decimal_input():
+    # Test invalid decimal input
+    invalid_inputs = [-1, 0, 4000, 1.5, "abc"]
+    for decimal in invalid_inputs:
+        with pytest.raises(RomanError):
+            RomanNumeral.from_decimal(decimal)
